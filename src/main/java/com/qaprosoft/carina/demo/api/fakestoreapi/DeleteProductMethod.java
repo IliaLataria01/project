f444fb9754582bb3fc164bb69e6705167838ec5f
IliaLataria01 <ilataria@solvd.com>
2022-12-30 15:14:29 +0400

    placed delete by 'id' to _testdata.properties and removed unused urls

--- src/main/java/com/qaprosoft/carina/demo/api/fakestoreapi/DeleteProductMethod.java
+++ src/main/java/com/qaprosoft/carina/demo/api/fakestoreapi/DeleteProductMethod.java
@@ -7,10 +7,14 @@ import com.qaprosoft.carina.core.foundation.api.annotation.ResponseTemplatePath;
 import com.qaprosoft.carina.core.foundation.api.annotation.SuccessfulHttpStatus;
 import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
 import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
+import com.zebrunner.carina.utils.R;
 
-@Endpoint(url = "${config.env.api_url_t}/products/${config.env.id}", methodType = HttpMethodType.DELETE)
+@Endpoint(url = "${config.env.api_url_t}/products/${product_id}", methodType = HttpMethodType.DELETE)
 @RequestTemplatePath(path = "api/products/fakestoreapi/_delete/rq.json")
-@ResponseTemplatePath(path = "api/product/fakestoreapi/_delete/rs.json")
+@ResponseTemplatePath(path = "api/products/fakestoreapi/_delete/rs.json")
 @SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
 public class DeleteProductMethod extends AbstractApiMethodV2 {
+    public DeleteProductMethod() {
+        replaceUrlPlaceholder("product_id", R.TESTDATA.get("product_id"));
+    }
 }
